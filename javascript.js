bf974fe6ab917242fede87a53176d16fc3f684e7
naufanabiil <naufalnabiil430@gmail.com>
2024-11-29 14:35:03 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,2 @@
+var hasil = document.getElementById("hasil-output");
+hasil.innerHTML = "<h1>Biodata</h1><p>Nama :naufal nabil kiram</p> <p>Kelas : XI RPL</p>";
---
+++ poppup.js
@@ -0,0 +1,8 @@
+confirm ("mauka kasih masuk nama ku?");
+var nama=prompt("masukkan mi nama nu?","")
+document.write("<p>hello"+nama+"</p>");
+document.write("<p>hello"+nama+"</p>");
+document.write("<p>hello"+nama+"</p>");
+document.write("<p>hello"+nama+"</p>");
+document.write("<p>hello"+nama+"</p>");
+alert("hello" + nama)
